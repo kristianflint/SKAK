abc37717089eedf361d06cc3f8cfb8dfeef09cb5
elektroels <elektroels@gmail.com>
2013-11-15 21:31:30 +0100

    Small changes #2
    en passent skal huskes

--- Chess/Chess/src/Board.java
+++ Chess/Chess/src/Board.java
@@ -5,9 +5,16 @@ public class Board{
     int[] board = new int[128];
     List<Piece> PiecesWhite;
     List<Piece> PiecesBlack;
+    boolean enPassant;
     
     //update board og piece
     public void movePiece(Move move){
         board[move.getPositionTo()] = board[move.getPositionFrom()];
+        
+        // Der skal tages højde for En passant på en eller anden måde
+        
     }
+    
+
+    
 }
