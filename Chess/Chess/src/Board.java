2916e40f2937c9fc6780b56f51954e49113174d8
elektroels <elektroels@gmail.com>
2013-11-15 18:15:30 +0100

    Small additions #1
    Small additions #1

--- Chess/Chess/src/Board.java
+++ Chess/Chess/src/Board.java
@@ -1,12 +1,13 @@
 import java.util.List;
 
 public class Board{
-
+    
     int[] board = new int[128];
     List<Piece> PiecesWhite;
     List<Piece> PiecesBlack;
     
     //update board og piece
     public void movePiece(Move move){
+        board[move.getPositionTo()] = board[move.getPositionFrom()];
     }
 }
--- Chess/Chess/src/Piece.java
+++ Chess/Chess/src/Piece.java
@@ -1,4 +1,29 @@
 public class Piece{
     int type;
     int position;   
+    
+    public Piece(int type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    public int getType() {
+        return type;
+    }
+    
+    /**
+     * Used for promotion(s)
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+        
 }
