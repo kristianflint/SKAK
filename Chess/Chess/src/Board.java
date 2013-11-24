1da41dbc823f8463c30afdea00efea1b52e8c168
kristianflint <kristian@nielss-MacBook-Pro.local>
2013-11-24 01:49:54 +0100

    Sliding Piece  done
    Alt med sliding piece er lavet og igang med at lave knigth der ikke er
    sleding. inden ændring fra SlidingRec til general

--- Chess/Chess/src/Board.java
+++ Chess/Chess/src/Board.java
@@ -19,17 +19,57 @@ public class Board{
      * 11 = Black Queen
      * 12 = Black King
      * 
+     * 
+     * 
+     * 
+     * var WHITE_PAWN = 0x01;
+     *var WHITE_KNIGHT = 0x02;
+     *var WHITE_KING = 0x03;
+     *var WHITE_BISHOP = 0x05;
+     *var WHITE_ROOK = 0x06;
+     *var WHITE_QUEEN = 0x07;
+      *
+     *var BLACK_PAWN = 0x09;
+     *var BLACK_KNIGHT = 0x0A;
+     *var BLACK_KING = 0x0B;
+     *var BLACK_BISHOP = 0x0D;
+     *var BLACK_ROOK = 0x0E;
+     *var BLACK_QUEEN = 0x0F;
+     *
+     * 
+     * 
+     * 
      * There are also 2 arrays used to identify pieces still on the board
      * Thus making it easier to identify active pieces 
      * That needs to have moves generated for them
      */
     
     int[] board = new int[128];
-    ArrayList<Piece> PiecesWhite;
+    ArrayList<Piece> PiecesWhite = new ArrayList();
     ArrayList<Piece> PiecesBlack;
+    
+    int BLACK_QUEEN = 0x0F;
+    int BLACK_BISHOP = 0x0D;
+    int BLACK_KNIGHT = 0x0A;
     
     boolean enPassant;      // der kan vel kun være 1 mulig en passant af gangen?
     int enPassantPosition;
+    
+    public Board() {
+        
+        Piece p0 = new Piece(BLACK_QUEEN, 3);
+        Piece p1 = new Piece(BLACK_QUEEN, 2);
+        Piece p2 = new Piece(BLACK_KNIGHT, 1);
+        
+        PiecesWhite.add(p0);
+        PiecesWhite.add(p1);
+        PiecesWhite.add(p2);
+        
+        board[1] = BLACK_KNIGHT;
+        board[2] = BLACK_BISHOP;
+        board[3] = BLACK_QUEEN;
+        
+    }
     
     //update board og piece
     public void movePiece(Move move) {
--- Chess/Chess/src/MoveGenerator.java
+++ Chess/Chess/src/MoveGenerator.java
@@ -29,9 +29,22 @@ public class MoveGenerator {
         0x0F, // one up one left
         0x10, // one up
         0x11};     // one up one right
-
+    
+    int BLACK_QUEEN = 0x0F;
+    int BLACK_ROOK = 0x0E;
+    int BLACK_BISHOP = 0x0D;
+    int BLACK_KNIGHT = 0x0A;
+    
+    
     // er de samme som kongens bare med voksende offset :-D 
-    int[] queenMoves = {};
+    int[] queenMoves = {0x01, // one right
+        -0x0F, // one down one right
+        -0x10, // one down
+        -0x11, // one down one left
+        -0x01, // one left
+        0x0F, // one up one left
+        0x10, // one up
+        0x11};     // one up one right
 
     int[] pawnMoves = {0x10, // one up
         0x11, // one up one right
@@ -48,9 +61,58 @@ public class MoveGenerator {
         -0x01, // one left
         0x10};    // one up
 
-    public boolean isValidMove() {
-        return true;
+    int[][] slidingMoves = {queenMoves,bishopMoves,rookMoves,knightMoves};
+    
+    int COMP_COLOR = 1; //0 = white 1 = black
+        
+    public boolean isValidMove(Board board, int currentPos, int offset, Piece me, boolean isFirst) {
+        int lastMove = currentPos - offset;
+        //System.out.println( "isValidMove");
+        if (!isFirst){
+             
+            if (((lastMove) & 0x88) == 0) { // inside board
+                if((board.board[lastMove] != 0) && (me.getPosition() == lastMove) ){// der stod en på sidste move
+                    System.out.println( "me.getPosition()" + me.getPosition());
+                    System.out.println( "lastMove" + lastMove);
+                    return false;
+                }
+            }
+        }
+        //System.out.println( "isValidMove");
+         if (((currentPos + offset) & 0x88) == 0) { // inside board
+
+            if ((board.board[(currentPos + offset)] & 0x8) != 0) { // det næste move er en af vores egne
+                return false;
+            }
+        
+             return true;
+         }
+        
+         return false;
+
+        
+        //return ((currentPos + offset) & 0x88) == 0;
     }
+    
+    
+    public int[] convertPiceTypeToIntList(int type) {
+        if (type == BLACK_QUEEN){
+            return slidingMoves[0];
+        }
+         if (type == BLACK_BISHOP){
+            return slidingMoves[1];
+        }
+         if (type == BLACK_ROOK){
+            return slidingMoves[2];
+        }
+         if (type == BLACK_KNIGHT){
+            return slidingMoves[3];
+        }         
+         
+        return slidingMoves[3];
+        //return ((currentPos + offset) & 0x88) == 0;
+    }
+    
     //Funktion som også tjekker i forhold til andres brikker
     //  Den funktion skal også tage højde for en passant (hvis nu Boardet kan huske om det var der)
 
@@ -58,6 +120,42 @@ public class MoveGenerator {
     //tjek om den slår sin egen brik.
     //Tjek udenfor
     // KOM SÅ GITHUBBBB
+    
+    
+    public Stack<Move> generateMovesSlidingPeaceRec(Board board, boolean player, Piece CurrPeace, int offset, boolean isFirst) {
+        Stack<Move> moves = new Stack<>();
+      
+                int currentPos = CurrPeace.getPosition();
+                Piece tempPeace = new Piece(CurrPeace.type,CurrPeace.position);
+                if (isValidMove(board, currentPos, offset, tempPeace, isFirst)) {
+                    int newpos = currentPos+offset;
+                    //System.out.println( "Newpos:" + Integer.toHexString(newpos));
+                    moves.add(new Move(currentPos, newpos,CurrPeace));
+                    tempPeace.setPosition(newpos);
+                    moves.addAll(generateMovesSlidingPeaceRec(board, player,tempPeace, offset, false));
+                }
+        return moves;
+    }
+    
+    public Stack<Move> generateMovesSlidingPeace(Board board, boolean player) {
+        Stack<Move> moves = new Stack<>();
+        
+        for (int index = 0; index < board.PiecesWhite.size(); index++) {
+            Piece currentPiece = board.PiecesWhite.get(index);
+            for (Integer offset : convertPiceTypeToIntList(currentPiece.type)) {
+                if ((currentPiece.getType() & 0x4) != 0) { // Is sliding pice
+                    moves.addAll(generateMovesSlidingPeaceRec(board, player,currentPiece, offset, true));
+                }else{
+                    //moves = generateMovesSingle(board, player,currentPiece, offset, true);
+                }
+                
+            }  
+        }
+        return moves; //return moves;
+        
+    }
+    
+    
     public Stack<Move> generateMoves(Board board, boolean player) {
         Stack<Move> moves = new Stack<>();
         /**
@@ -74,7 +172,7 @@ public class MoveGenerator {
             if (currentType == 6) { // (12|6)) { // if its a king
                 int currentPos = board.PiecesWhite.get(index).getPosition();
                 for (Integer offset : kingMoves) {
-                    if (((currentPos + offset) & 0x88) == 0) { // Piece is inside the board!
+                    //if (isValidMove(board, currentPos, offset, true)) { // Piece is inside the board!
                         int nextPosContains = board.board[(currentPos + offset)];
                         int nextPos = currentPos + offset;
                         if (nextPosContains < 7) {
@@ -97,7 +195,7 @@ public class MoveGenerator {
                             System.out.println("hejhejhej");
                             moves.add(new Move(currentPos, nextPos, currentPiece)); }
                     }
-                }
+                //}
             }
         }
         }
@@ -105,5 +203,24 @@ public class MoveGenerator {
         else {}
         return moves;
     }
+
+    
+           
+public static void main (String[] args) {
+System.out.println( "WHAT!?");
+Stack<Move> moves = new Stack();
+        
+MoveGenerator tester = new MoveGenerator();
+Board myBoard = new Board();
+Move tempMove;
+
+moves = tester.generateMovesSlidingPeace(myBoard, true);
+
+
+while (moves.size() > 0){
+  tempMove = moves.pop();
+  System.out.println(Integer.toHexString(tempMove.getPositionTo()));  
+}
+}
 
 }
