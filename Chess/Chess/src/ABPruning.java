56d35448e40d85da7e4714aa179ab5a1a7e8441b
kristianflint <kristian@nielss-MacBook-Pro.local>
2013-12-01 19:54:38 +0100

    Nu går vi hjem

--- Chess/Chess/src/ABPruning.java
+++ Chess/Chess/src/ABPruning.java
@@ -1,7 +1,8 @@
-package TicTacToe;
+
 
 import java.util.Stack;
-        
+
+   
 public class ABPruning
 {
   int alpha;
@@ -13,63 +14,72 @@ public class ABPruning
  // Board newBoard = new Board();
   private int score;
   private int best_score;
+  private Evaluator myEvaluator = new Evaluator();
+  private MoveGenerator myMoveGenerator = new MoveGenerator();
+         
+public static void main (String[] args) {
+
+  Board newBoard = new Board();
+  newBoard.populateMe();
+  ABPruning newABPruning = new ABPruning();
+  newABPruning.pruning(newBoard);
+    
+}
 
   public void pruning(Board board){
       
-      System.out.println("Oprettet et AB board objekt... ");
-      System.out.println("Printer objektet: ");
-      board.printBoard();
-      System.out.println("Færdig med at printe");
-      miniMax(9, Board.player, board);     
+      miniMax(1, 1, board);     
   
   }
   
   public int miniMax(int depth, int turn, Board board)
 	{  
+            boolean endOfGAME = false;
+            
             Stack moves = new Stack();
-            if (moves.empty()){
-                  switch (board.getWinner()){
+            if (endOfGAME){
+                  switch (4){
                    case 1:  return inf;
                    case 2:  return -inf;
                    case 3:  return 0;                    
                 }
             }
-            else{
-                for (Object move : moves) {
-                best_score = inf;                
-                Board newBoard = new Board();
-                newBoard = board;
-                moves.add(move);                
-                if (turn == 1){                    
-                    score = alphaBetaMax(alpha, beta, depth);                               
-                    }
-                else{
-                    score = alphaBetaMin(alpha, beta, depth);                               
-                }               
+            if (depth == 0){
+                return myEvaluator.evaluateBoard(board, turn);
             }
+            
+            moves = myMoveGenerator.generateMoves(board, true, turn);
+
                 
-               /* 
-                 * apply_move(new_board, moves_list[i]);
-                minimax(new_board, depth+1, max_depth, the_score, the_move);
-                if (better(the_score, best_score)) then
-                    best_score = the_score;
-                    best_move = the_move;
-                endif
-            enddo
-            chosen_score = best_score;
-            chosen_move = best_move;
-        endif
-                
-                */                
-        
-    }       
+                for (Object move : moves) {
+                    best_score = inf;                
+                    Board newBoard = new Board();
+                    newBoard = board.copyMe();
+                    
+                    newBoard.movePiece((Move)move);
+                    
+                    if (turn == 1) score = alphaBetaMax(alpha, beta, depth-1, turn, newBoard);                               
+                    else score = alphaBetaMin(alpha, beta, depth-1, turn, newBoard);                               
+                }    
+  
             return score;
  }
                            
-public int alphaBetaMax( int alpha, int beta, int depth) {
+public int alphaBetaMax( int alpha, int beta, int depth, int turn, Board board) {
    Stack moves = new Stack();
+   
+   if (depth == 0){
+    return myEvaluator.evaluateBoard(board, turn);
+   }
+   
+   turn=(turn==1)?0:1;
+   moves = myMoveGenerator.generateMoves(board, true, turn);
+   Board newBoard = new Board();
+   newBoard = board.copyMe();
+   
     for (Object move : moves) {
-      score = alphaBetaMin( alpha, beta, depth - 1);
+      newBoard.movePiece((Move)move);
+      score = alphaBetaMin( alpha, beta, depth - 1, turn, newBoard);
       if( score >= beta )
          return beta;
       if( score > alpha )
@@ -78,10 +88,20 @@ public int alphaBetaMax( int alpha, int beta, int depth) {
    return alpha;
 }
  
-public int alphaBetaMin( int alpha, int beta, int depth) {  
+public int alphaBetaMin( int alpha, int beta, int depth, int turn, Board board) {  
    Stack moves = new Stack();
-    for (Object move : moves) {   
-       score = alphaBetaMax( alpha, beta, depth - 1);
+   
+   if (depth == 0){
+    return myEvaluator.evaluateBoard(board, turn);
+   }
+   
+   turn=(turn==1)?0:1;
+   moves = myMoveGenerator.generateMoves(board, true, turn);
+   Board newBoard = new Board();
+   newBoard = board.copyMe();
+    for (Object move : moves) {
+        newBoard.movePiece((Move)move);
+       score = alphaBetaMax( alpha, beta, depth - 1, turn, newBoard);
       if( score <= alpha )
          return alpha;
       if( score < beta )
@@ -89,9 +109,7 @@ public int alphaBetaMin( int alpha, int beta, int depth) {
    }
    return beta;
 }
-        
-    private int evaluate(Board board) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+
 }
+
 
